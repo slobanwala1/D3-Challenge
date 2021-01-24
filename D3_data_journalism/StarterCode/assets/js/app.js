d7c9c679c1e7d2480d96d9a2bb6e6f57e7dbb420
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-01-23 22:58:14 -0600

    Fixed chart display bug

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -4,20 +4,8 @@
 // Project Name: D3-Challenge
 // File: Main logic/app.js
 
-
-// Read csv in
-d3.csv("assets/data/data.csv", function(data){
-
-  // Convert poverty value and healthcare to int instead of string aka = +(string)
-  data.poverty = +data.poverty;
-  data.healthcare = +data.healthcare;
-  return data;
-}).then(function(data){
-  // All the data in nice objects
-  // console.log(data);
-})
-
-// Create the svg
+//---------------------------------------------------------------------------------------------------------------------
+// Create the svg dimensions and margins
 var widthPreMargins = 800;
 
 var heightPreMargins = 500;
@@ -38,3 +26,37 @@ var svgWrapper = d3.select("#scatter").append("svg").attr("width", widthPreMargi
 
 // svg group to smash together the different elements
 var svgChartGroup = svgWrapper.append("g").attr("transform", `translate(${svgMargins.left}, ${svgMargins.top})`);
+//---------------------------------------------------------------------------------------------------------------------
+// Read csv in
+d3.csv("assets/data/data.csv", function(data){
+
+  // Convert poverty value and healthcare to int instead of string aka = +(string)
+  data.poverty = +data.poverty;
+  data.healthcare = +data.healthcare;
+  return data;
+}).then(function(data){
+  // All the data in nice objects
+  // console.log(data);
+
+  // Start manipulating the chart with the data.
+  // create the x linearscales and y slinearscales
+  // var xLinScale = d3.scaleLinear().domain([9, d3.max(data, function(d){
+  //   return +d.poverty;
+  // })]).range([0, widthPostMargins]);
+  var xLinScale = d3.scaleLinear().domain([9, d3.max(data, function(d){
+    return +d.poverty;
+  })]).range([0, widthPostMargins]);
+
+
+  var yLinScale = d3.scaleLinear().domain([3, d3.max(data, function(d){
+    return +d.healthcare;
+  })]).range([heightPostMargins, 0]);
+
+  // We need the xy axis aka bottom and left using the linear scales ^^^
+  var xAxis = d3.axisBottom(xLinScale);
+  var yAxis = d3.axisLeft(yLinScale);
+
+  // append the axises to our svgChartGroup
+  svgChartGroup.append("g").attr("transform", `translate(0, ${heightPostMargins})`).call(xAxis);
+  svgChartGroup.append("g").call(yAxis);
+})
